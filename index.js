ad344910b84e332927ed5d9a5700f140a87c08e3
barikassa <barikassa01@gmail.com>
2021-08-16 21:43:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,135 @@
+
+//!-- 1.
+// username1=prompt("write your firstname");
+// username2= prompt("write your lastname"); 
+// function getTheLongerName(firstName, lastName){ 
+    
+//     if(username1.length>username2.length){
+//         console.log(username1);
+//     }
+//     else{
+//         console.log(username2);
+//     }
+// }
+
+// let result= getTheLongerName("xs","tt");
+
+//!2. 
+
+// var firstName=prompt("write your name");
+// var lastName=prompt("write your name ");
+
+// if(firstName.indexOf("a")!=-1){
+//     console.log(firstName);
+// }
+// else{
+//     console.log("doesnt exist");
+// }
+// if (lastName.indexOf("api")!=-1){
+// console.log(lastName);
+// }
+// else{
+//     console.log("doesnt exist");
+// }
+
+//!-- 3. 
+
+
+// let fullName=prompt(("please write your name"));
+// function namePrinter(contractors){
+//         console.log(contractors.length);
+// } 
+// console.log(namePrinter(fullName));
+
+
+
+// let firstName=prompt(("fname"));
+// function getFirstName (printFirstname){
+//     let spaceIndex=printFirstname.indexof(" ");
+//     let firstHalf= printFirstname.substring(0,spaceIndex);
+//     return printFirstname;
+// }
+// let result= getFirstName(printFirstname);
+// console.log(printFirstname);
+
+
+
+//!------ 4. 
+// let userInput=prompt(("write your name"));
+// function isFiveString(fivest){
+//     if(userInput.length>5){
+//         return "long";
+//     }
+//     else{
+//         return "short";
+//     }
+// }
+// let result1= isFiveString(userInput);
+
+// console.log(result1);
+//!!--5. 
+// function getCountryNameByString(shortNameCountry){
+//     if(shortNameCountry.length<3){
+//         return "yes"
+//     }
+//     else{
+//         return "no"
+//     }
+// }
+// let result = getCountryNameByString("vietname");
+// console.log(result);
+//!--- 6. 
+
+// function longString(sixString, letterString) {
+//     if ( sixString.length > 6) {
+//         return sixString;
+//     } 
+//     else if(letterString.indexOf([0])==sixString.subStr) {
+
+//     } return letterString;
+
+// }
+// let result = longString("uoup", "p");
+// console.log(result);
+
+//!--- 7. 
+
+// function getStringAndLetter(letterString, stringSix){
+//     for( let i=0; i<stringSix.length; i++){
+//        if(stringSix[i]==letterString[i]){
+           
+//        }return letterString;
+//     }
+//     return stringSix;
+// }
+//  let result = getStringAndLetter("yy","rrty");
+//  console.log(result);
+//!---8. 
+// function fullName(firstName,lastName){
+//     return firstName + " "+ lastName.toUpperCase();  
+// }
+// let result = fullName("daniel","criger");
+// console.log(result);
+//!-- 9. 
+// function getlongerfamilyName(familyName1, familyName2){
+//     if(familyName1.length>familyName2.length){
+//      return familyName1;  
+//     }
+//     else{
+//         return familyName2.toLowerCase() ;
+//     }
+// }
+// let result= getlongerfamilyName("kassa","casies");
+// console.log(result);
+//!--10. 
+// function nameAndletter(nameAll, laterAll){
+//      for(let i=0;i<nameAll.length;i++){
+//          if(laterAll[i]==nameAll[i]){
+//             return nameAll; 
+//          }
+
+//          return "please write your name";
+//         }   
+// }
+// let result = nameAndletter("tyy","ppppp");
+// console.log(result);
